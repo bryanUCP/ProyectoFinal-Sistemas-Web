584725171a1c61e8797cc8de0369d81abe4a419c
bryanUCP <bryanpiq2600@gmail.com>
2021-12-07 00:10:38 -0600

    Modificaciones

--- ProyectoFinal/src/main/java/mx/uv/App.java
+++ ProyectoFinal/src/main/java/mx/uv/App.java
@@ -14,32 +14,18 @@ import mx.uv.BD.Usuario;
  * Hello world!
  *
  */
+
 public class App 
 {
     private static Gson gson = new Gson();
 
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
-
-        options("/*", (request, response) -> {
-
-            String accessControlRequestHeaders = request.headers("Access-Control-Request-Headers");
-            if (accessControlRequestHeaders != null) {
-                response.header("Access-Control-Allow-Headers", accessControlRequestHeaders);
-            }
-
-            String accessControlRequestMethod = request.headers("Access-Control-Request-Method");
-            if (accessControlRequestMethod != null) {
-                response.header("Access-Control-Allow-Methods", accessControlRequestMethod);
-            }
-
-            return "OK";
-        });
-
-        before((request, response) -> response.header("Access-Control-Allow-Origin", "*"));
+        port(1234);
+        staticFiles.location("/");
 
         get("/", (req, res) -> {
+            res.redirect("/login.html");
             return null;
         });
 
